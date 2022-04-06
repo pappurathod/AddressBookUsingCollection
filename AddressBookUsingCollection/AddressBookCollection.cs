6b00462e013588a22426defcdc18515b4b34ed05
pappurathod <prathod4545@gmail.com>
2022-04-06 14:56:01 +0530

    Recorrection

---
+++ AddressBookUsingCollection/AddressBookCollection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookUsingCollection
+{
+    class AddressBookCollection
+    {
+        public Dictionary<string, AddressBook> addressBookDictionary;//Dictionary collection
+        public AddressBookCollection()
+        {
+            addressBookDictionary = new Dictionary<string, AddressBook>();
+        }
+        public void PrintAllAddressBookNames()
+        {
+            foreach (var AddressBookItem in addressBookDictionary)
+            {
+                Console.WriteLine(AddressBookItem.Key);
+            }
+        }
+        public void SearchPersonInCityOrState(string firstName, string lastName)
+        {
+            foreach (var addressBookEntry in addressBookDictionary)
+            {
+                List<Person> PersonInCitiesOrStates = addressBookEntry.Value.addressBook.FindAll(i => (i.firstName == firstName) && (i.lastName == lastName));
+                foreach (Person person in PersonInCitiesOrStates)
+                {
+                    Console.WriteLine($" {person.firstName} {person.lastName} is in {person.city} {person.state}");
+                }
+            }
+        }
+    }
+}
+
